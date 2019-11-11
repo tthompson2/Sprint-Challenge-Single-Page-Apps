f6dc5b64004ed651d9d1feaa2ece410a9469b759
Trevor Thompson <trevthom18@gmail.com>
2019-11-11 18:58:21 -0500

    Added styled components to the characters page

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,16 +1,27 @@
 import React from "react";
 
+import styled from 'styled-components';
+
+const WrapperDiv = styled.div `
+
+   width: 100%
+   color: green
+   height: 100%
+   font-family: Fantasy
+
+`;
+
 const CharacterCard = props => {
 
  return (
 
-  <div>
+  <WrapperDiv>
     {props.id}
     {props.status}
     {props.name}
     {props.species}
-  </div>
-  )
+    </WrapperDiv>
+ )
 }
 
 export default CharacterCard;
