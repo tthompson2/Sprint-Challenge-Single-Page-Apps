ce0ca44efd9a96b34a2b0b360595ece0c5f76285
Trevor Thompson <trevthom18@gmail.com>
2020-01-24 21:29:46 -0500

    Implemented incredibly rundmentary styling so that I can meet MVP

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,21 +1,46 @@
 import React from "react";
 
+import styled from 'styled-components';
+
+const Alert = styled.div`
+
+    ${props => (props.color === 'primary' ? `background: #ff0000;` : null)}
+    ${props => (props.color === 'secondary' ? `background: #00FF00;` : null)}
+    ${props => (props.color === 'success' ? `background: #0000FF;` : null)}
+    ${props => (props.color === 'danger' ? `background: #ffFF00;` : null)}
+    ${props => (props.color === 'warning' ? `background: #ff00FF;` : null)}
+    ${props => (props.color === 'info' ? `background: #00FFFF;` : null)}
+    ${props => (props.color === 'light' ? `background: #f00000;` : null)}
+`;
+
 export default function CharacterCard(props) {
 
-  const {image, id, name, status, species, type} = props.character;
+  const { image, id, name, status, species, type } = props.character;
 
-  return ( 
-  <div>
-    {image}
-    <br/>
-    {id}
-    {name}
-    {status}
-    {species}
-    {type}
-    <br/>
-    <br/>
-  </div>
+  return (
+    <div>
+      <Alert color="primary">
+        {image}
+        <br />
+      </Alert>
+      <Alert color="secondary">
+        {id}
+      </Alert>
+      <Alert color="success">
+        {name}
+      </Alert>
+      <Alert color ="warning">
+        {status}
+      </Alert>
+      <Alert color="info">
+        {species}
+      </Alert>
+      <Alert color="light">
+        {type}
+      </Alert>
+      <br />
+      <br />
+    </div>
 
   );
 }
